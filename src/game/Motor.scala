8287b496e812c6b57bb7f18b06cefbc6daf48924
Allan Brunner <allan@allanbrunner.dev>
2025-04-19 23:06:34 +0200

    Adds lap timing and display
    Adds lap timing functionality to track player lap times and best times.
    Includes on-screen display of current lap time, total time, best lap, and last lap times.
    Introduces a `LapTimes` data structure to manage lap data.
    Adds font assets for on-screen display.
    Replaces PlayerCheckpointTracker with Player, which now handles lap timing.
    Adds Conversion util to convert time in milliseconds to a human-readable string.

--- src/game/Motor.scala
+++ src/game/Motor.scala
@@ -5,9 +5,13 @@ import ch.hevs.gdx2d.lib.GdxGraphics
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input.Keys
 import com.badlogic.gdx.backends.lwjgl.{LwjglApplication, LwjglApplicationConfiguration}
+import com.badlogic.gdx.files.FileHandle
+import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator
 import com.badlogic.gdx.graphics.{Color, Texture}
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.badlogic.gdx.utils.Align
 import com.badlogic.gdx.utils.viewport.ScreenViewport
 import utils.GraphicsUtils
 
@@ -50,13 +54,18 @@ class Motor(var width: Int, var height: Int, fullScreen: Boolean) extends Portab
   private var kart: Kart = _
   private var camera: Camera = _
   private var track: Track = _
-  private var playerCheckpointTracker: PlayerCheckpointTracker = _
+  private var player: Player = _
 
   private var forward: Boolean = false
   private var backward: Boolean = false
   private var left: Boolean = false
   private var right: Boolean = false
   private var drift: Boolean = false
+
+  private var debug: Boolean = true
+
+  private var totalTimeFont: BitmapFont = _
+  private var lapTimeFont: BitmapFont = _
 
   override def onInit(): Unit = {
     stage = new Stage(new ScreenViewport())
@@ -68,14 +77,17 @@ class Motor(var width: Int, var height: Int, fullScreen: Boolean) extends Portab
     track = new Track("map_1")
     initKart()
     camera = new Camera()
-    playerCheckpointTracker = new PlayerCheckpointTracker(track)
+    player = new Player(track)
 
     mode7Renderer = new Mode7Renderer(track.mapTexture)
+
+    initFonts()
 
     stage.clear()
   }
 
   override def onGraphicRender(g: GdxGraphics): Unit = {
+
     g.clear()
 
     // Fills the top of the screen with sky color (from top to horizon)
@@ -101,7 +113,7 @@ class Motor(var width: Int, var height: Int, fullScreen: Boolean) extends Portab
 
     val segmentInfo = track.closestSegmentAndProgress(kart.x, kart.y)
 
-    playerCheckpointTracker.update(segmentInfo._1, kart)
+    player.update(segmentInfo._1, kart)
 
     camera.update(kart)
 
@@ -109,21 +121,10 @@ class Motor(var width: Int, var height: Int, fullScreen: Boolean) extends Portab
 
     g.drawTransformedPicture(width / 2, height / 4, 0, 3, kart.texture)
 
-    g.drawString(10, 20, "Laps: " + playerCheckpointTracker.lapsCompleted)
-    g.drawString(10, 40, "Segment: " + segmentInfo._1)
-    g.drawString(10, 60, "SegDist: " + segmentInfo._2)
-    g.drawString(10, 80, "TotDist: " + segmentInfo._3)
-    g.drawString(10, 100, "DistPer: " + segmentInfo._4)
+    g.drawStringCentered(150, player.lapTime, lapTimeFont)
+    g.drawStringCentered(80, player.totalTime, totalTimeFont)
 
-    g.drawString(10, 220, "Speed: " + kart.speed.toString)
-    g.drawString(10, 240, "X: " + kart.x.toString)
-    g.drawString(10, 260, "Y: " + kart.y.toString)
-    g.drawString(10, 280, "Angle: " + kart.angle.toString)
-
-    g.drawString(10, 320, "X: " + camera.x.toString)
-    g.drawString(10, 340, "Y: " + camera.y.toString)
-    g.drawString(10, 360, "Angle: " + camera.angle.toString)
-
+    if (debug) displayDebug(g, segmentInfo)
     GraphicsUtils.drawFPS(g, Color.WHITE, 5f, height - 10)
 
     // Update and render UI
@@ -143,6 +144,70 @@ class Motor(var width: Int, var height: Int, fullScreen: Boolean) extends Portab
     }
   }
 
+  override def onKeyDown(keycode: Int): Unit = {
+    super.onKeyDown(keycode)
+
+    keycode match {
+      case Keys.W => forward = true
+      case Keys.S => backward = true
+      case Keys.A => left = true
+      case Keys.D => right = true
+      case Keys.SHIFT_LEFT => drift = true
+      case Keys.ENTER => player.startLap()
+      case _ =>
+    }
+  }
+
+  override def onKeyUp(keycode: Int): Unit = {
+    super.onKeyUp(keycode)
+
+    keycode match {
+      case Keys.W => forward = false
+      case Keys.S => backward = false
+      case Keys.A => left = false
+      case Keys.D => right = false
+      case Keys.SHIFT_LEFT => drift = false
+      case _ =>
+    }
+  }
+
+  def initFonts(): Unit = {
+    val consola = Gdx.files.internal("assets/fonts/consola.ttf")
+    val generator = new FreeTypeFontGenerator(consola)
+
+    val paramTotalTime = new FreeTypeFontGenerator.FreeTypeFontParameter
+    paramTotalTime.color = Color.WHITE
+    paramTotalTime.size = generator.scaleForPixelHeight(36)
+    paramTotalTime.hinting = FreeTypeFontGenerator.Hinting.Full
+    totalTimeFont = generator.generateFont(paramTotalTime)
+
+    val paramLapTime = new FreeTypeFontGenerator.FreeTypeFontParameter
+    paramLapTime.color = Color.WHITE
+    paramLapTime.size = generator.scaleForPixelHeight(72)
+    paramLapTime.hinting = FreeTypeFontGenerator.Hinting.Full
+    lapTimeFont = generator.generateFont(paramLapTime)
+  }
+
+  def displayDebug(g: GdxGraphics, segmentInfo: (Int, Float, Float, Float)): Unit = {
+    g.drawString(10, 20, "Laps: " + player.lapsCompleted)
+    g.drawString(10, 40, "Segment: " + segmentInfo._1)
+    g.drawString(10, 60, "SegDist: " + segmentInfo._2)
+    g.drawString(10, 80, "TotDist: " + segmentInfo._3)
+    g.drawString(10, 100, "DistPer: " + segmentInfo._4)
+
+    g.drawString(10, 220, "Speed: " + kart.speed.toString)
+    g.drawString(10, 240, "X: " + kart.x.toString)
+    g.drawString(10, 260, "Y: " + kart.y.toString)
+    g.drawString(10, 280, "Angle: " + kart.angle.toString)
+
+    g.drawString(10, 320, "X: " + camera.x.toString)
+    g.drawString(10, 340, "Y: " + camera.y.toString)
+    g.drawString(10, 360, "Angle: " + camera.angle.toString)
+
+    g.drawString(10, 400, "Best Lap: " + player.bestLap)
+    g.drawString(10, 420, "Last Lap: " + player.lastLap)
+  }
+
   private def createLwjglApplication(): Unit = {
     Thread.currentThread.setPriority(10)
 
@@ -159,32 +224,6 @@ class Motor(var width: Int, var height: Int, fullScreen: Boolean) extends Portab
 
     val theGame = new Game2D(this)
     new LwjglApplication(theGame, config)
-  }
-
-  override def onKeyDown(keycode: Int): Unit = {
-    super.onKeyDown(keycode)
-
-    keycode match {
-      case Keys.W => forward = true
-      case Keys.S => backward = true
-      case Keys.A => left = true
-      case Keys.D => right = true
-      case Keys.SHIFT_LEFT => drift = true
-      case _ =>
-    }
-  }
-
-  override def onKeyUp(keycode: Int): Unit = {
-    super.onKeyUp(keycode)
-
-    keycode match {
-      case Keys.W => forward = false
-      case Keys.S => backward = false
-      case Keys.A => left = false
-      case Keys.D => right = false
-      case Keys.SHIFT_LEFT => drift = false
-      case _ =>
-    }
   }
 
   /**
